1167095e4ef2a165a2e27c654764e9e72cefa048
Rouncer27 <trevor@switchbackcreative.ca>
2022-12-05 07:04:46 -0700

    headers est contact

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,6 @@ module.exports = {
           ],
           "/contact-us": [
             "cache-control: public,max-age=60",
-            "X-Frame-Options: deny",
             "X-XSS-Protection: 1; mode=block",
             "X-Content-Type-Options: nosniff",
             "Feature-Policy: camera 'none'; geolocation 'none'; microphone 'none'",
